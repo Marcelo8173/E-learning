109faa818aa702a2a7767d184eeb3af4a78b3c5e
Marcelo8173 <marceloandrebio@gmail.com>
2021-01-20 23:11:09 -0300

    create style of buttons nav

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import {Container} from './styled';
+import React, { useState, useCallback } from 'react';
+import {BiHomeAlt} from 'react-icons/bi';
+import {AiOutlineHeart} from 'react-icons/ai';
+import {Container,Button} from './styled';
 
 const NavBar: React.FC = () => {
+
+    const [selected, setSelected] = useState(1);
+    
+    const handleSelectTab = useCallback((id:number) => {
+        setSelected(id);
+    },[])
+
     return(
         <Container>
-            olá
+            <Button selected={selected === 1 ? true : false} onClick={() => handleSelectTab(1)}>
+                <BiHomeAlt size={17} />
+                Home</Button>
+            <Button selected={selected === 2 ? true : false} onClick={() => handleSelectTab(2)}>
+                <AiOutlineHeart size={17}/>
+                Salvos</Button>
         </Container>
     )
 }
--- src/components/navbar/styled.ts
+++ src/components/navbar/styled.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
+interface IProps{
+    selected: boolean;
+}
+
 export const Container = styled.div`
+    margin-left: 24px;
+    display: flex;
+    button{
+        justify-content: center;
+        justify-items: center;
+    }
+`;
+
+export const Button = styled.button<IProps>`
+    
+    font-family: 'Roboto',sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    color: ${props => props.selected ? "#FF6680": "#C4C4D1"};
+    padding: 20px;
+    border-radius: 16px;
+    border: ${props => props.selected ? "3px solid red": '1px solid transparent'};
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;;
+    border-bottom: 1px solid transparent;;
+    margin-right: 16px;
+    svg{
+        margin-right: 5px;
+    }
     
 `;
