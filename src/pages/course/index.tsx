0ab0ca39d8a9727c489eca32113820343103ac62
Marcelo8173 <marceloandrebio@gmail.com>
2021-02-02 21:42:46 -0300

    create request in page course

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -3,21 +3,54 @@ import { Container } from './styles';
 import { useParams } from 'react-router-dom';
 import api from '../../services/api';
 import { ICousrses } from '../dashboard';
+import Header from '../../components/header';
+import Background from '../../components/Background';
+import { TitleContainer } from '../dashboard/style';
+
+interface IParams{
+    id: string;
+}
+
+interface ILesson{
+    description: string;
+    duration: string;
+    id: string;
+    name: string;
+}
 
 const Courses: React.FC = () => {
-    const id = useParams();
+    const {id} = useParams<IParams>();
 
     const [course,setCourse] = useState<ICousrses>();
+    const [lesson, setLesson] = useState<ILesson[]>([]);
 
     useEffect(() => {
         api.get(`/courses/${id}`).then(response => {
-            setCourse(response.data);
+            setCourse(response.data[0]);
         });
-    }, []);
-    
+        api.get(`/lesson/${id}`).then(response => {
+            setLesson(response.data)
+        })
+    }, [id]);
+
     return (
         <Container>
-            
+            <Header />
+            <main>
+                <Background>
+                    <TitleContainer>
+                        <h2>{course?.name}</h2>
+                        <span>43 cursos</span>
+                    </TitleContainer>
+                    <div className="grid">
+                        {lesson.map(item => (
+                            <div>
+                                {item.name}
+                            </div>
+                        ))}
+                    </div>
+                </Background>
+            </main>
         </Container>
     );
 }
--- src/pages/course/styles.ts
+++ src/pages/course/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    .grid{
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+    }
 `;
