26d6828854e1a2c3e6de4b5685fe029cf9c06ecd
Marcelo8173 <marceloandrebio@gmail.com>
2021-02-17 21:51:00 -0300

    initial layout

--- src/components/InternalCard/index.tsx
+++ src/components/InternalCard/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container,ContentButton,SectionImg } from './styles';
 
 const InternalCard = () => {
     return (
         <Container>
-            
+            <ContentButton>
+                <SectionImg>
+                    <img src="https://www.publicidadenaweb.com/wp-content/uploads/2019/06/como-fazer-video-de-vendas.jpg" alt="" />
+                </SectionImg>
+                <div>
+                    <h1>Titulo da materia</h1>
+                </div>
+            </ContentButton>
         </Container>
     );
 }
--- src/components/InternalCard/styles.ts
+++ src/components/InternalCard/styles.ts
@@ -1,5 +1,31 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
+export const ContentButton = styled.button`
+    margin: 0 24px 16px 61px;
+    display: flex;
+    text-decoration: none;
+    border: 1px solid transparent;
+    background: #FFFFFF;
+    border-radius: 16px;
+    align-items: center;
+    padding: 12px;
+    /* justify-content: center; */
+    width: 80%;
+`;
+
+export const SectionImg = styled.div`
+    /* position: absolute; */
+    width: 68px;
+    overflow: hidden;
+    border-radius: 16px;
+    img{
+        width: 68px;
+        height: auto;
+    }
 `;
